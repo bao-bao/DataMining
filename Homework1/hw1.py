746da833431125a7f1a98be923692f99004d29b7
包天宇 <1452765@tongji.edu.cn>
2017-03-28 16:05:37 +0800

    update gitignore

--- Homework1/hw1.py
+++ Homework1/hw1.py
@@ -2,55 +2,43 @@
  hw1.py Created by baobao on 3/15/17.
 """
 import pandas as pd
-import numpy
+import numpy as np
 import time
 from lshash import LSHash
 from sklearn.neighbors import NearestNeighbors
 
 data = pd.read_csv("Traj_1000_SH_UTM")
-print data
 
-x = 840
-y = 760
-length = 1000
 target = 69
-'''
-    target is the trace for lsh and knn
-'''
+temparray = np.zeros((840 * 760))
+matrix = np.zeros((1000, 44107))
 
-matrix = [[0 for i in range(44107)] for i in range(1000)]
-tempArray = [0 for i in range(x * y)]
-
-'''
-timelist = list(data["Time"])
-for d in timelist:
-    timeArray = time.strptime(d, "%Y-%m-%d %H:%M:%S")
-    timestamp = int(time.mktime(timeArray))
-    data.iloc[i, 1] = timestamp
-    '''
 for index, row in data.iterrows():
-    tempArray[int((row[3] - 3448600) / 20) * int((row[2] - 346000) / 20)] = 1
+    temparray[int((row[2] - 346000) / 20) * 760 + int((row[3] - 3448600) / 20)] = 1
 
-i = 1
-for a in tempArray:
-    if a == 1:
-        a = i
-        i += 1
+i = 0
+count = 0
+while i < 638400:
+    if temparray[i] == 1:
+        temparray[i] = count
+        count += 1
+    i += 1
 
-Tid = 0
-while Tid < 1000:
-    trace = data.loc[(data['Tid'] == Tid + 1), ['Time', 'X', 'Y']]
+tid = 0
+while tid < 1000:
+    trace = data.loc[(data['Tid'] == tid + 1), ['Time', 'X', 'Y']]
     for index, row in trace.iterrows():
-        matrix[Tid][tempArray[int((row[2] - 3448600) / 20) * int((row[1] - 346000) / 20)]] = 1
-    Tid += 1
+        matrix[tid][temparray[int((row[1] - 346000) / 20) * 760 + int((row[2] - 3448600) / 20)]] = 1
+    tid += 1
 
-lsh = LSHash(20000, 44107)
+lsh = LSHash(2000, 44107)
 for element in matrix:
     lsh.index(element)
 result = lsh.query(matrix[target - 1])
 for row in result:
-    print row
+    save = pd.DataFrame(row)
+    save.to_csv('hw1.csv')
 
-nbrs = NearestNeighbors(n_neighbors=20)
+nbrs = NearestNeighbors(n_neighbors=5)
 nbrs.fit(matrix)
 print nbrs.kneighbors(matrix[target - 1])
