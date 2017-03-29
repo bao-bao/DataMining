7167c91fe91841b1006787adc311f8d96d85a4a7
包天宇 <1452765@tongji.edu.cn>
2017-03-29 14:37:29 +0800

    update kmeans

--- Homework2/Kmeans.py
+++ Homework2/Kmeans.py
@@ -1,6 +1,32 @@
-import pandas as pd
 import numpy as np
-from Util import *
+import pandas as pd
+from sklearn import metrics
+from sklearn.cluster import KMeans
 
-matrix = ReadTraj_1000_SH_UTM.read()
-print matrix
+
+def kmeans(x, metric='euclidean'):
+    best_silhouette = -1
+    best_clusters = 0
+    cluster_silhouette = np.zeros((50, 2))
+
+    for clusters in range(22, 23, 1):
+        print 'Begin KMeans for cluster = %d' % clusters
+        # do KMeans and calculate silhouette
+        clt = KMeans(n_clusters=10)
+        clt.fit(x)
+        silhouette = metrics.silhouette_score(x, clt.labels_, metric=metric)
+
+        # make map for (n_cluster, silhouette)
+        cluster_silhouette[clusters][0] = clusters
+        cluster_silhouette[clusters][1] = silhouette
+
+        # get best n_clusters in KMeans
+        if silhouette > best_silhouette:
+            best_silhouette = silhouette
+            best_clusters = clusters
+
+    # out best cluster result to csv
+    best_kmeans = KMeans(n_clusters=best_clusters)
+    best_kmeans.fit(x)
+
+    return best_clusters, best_silhouette, best_kmeans.labels_
