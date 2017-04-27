b722115bd5b89158c05c65bddc180ecfe530e941
包天宇 <1452765@tongji.edu.cn>
2017-04-27 19:17:05 +0800

    read csv to dataset

--- Homework3/Homework3-2/ReadData.py
+++ Homework3/Homework3-2/ReadData.py
@@ -1,3 +1,36 @@
 """
     Created by AMXPC on 2017/4/27.
 """
+import pandas as pd
+
+
+def readcsv_notime(x):
+    dataframe = pd.read_csv(x)
+    dic = dict()
+
+    for index, row in dataframe.iterrows():
+        if row[1] not in dic:
+            dic[row[1]] = []
+        if row[47] not in dic[row[1]]:
+            dic[row[1]].append(row[47])
+
+    # print dic
+    return dic
+
+
+def readcsv_withtime(x):
+    dataframe = pd.read_csv(x)
+    dic = dict()
+    time = ''
+
+    for index, row in dataframe.iterrows():
+        if row[1] not in dic:
+            dic[row[1]] = []
+        if row[0] != time:
+            dic[row[1]].append([])
+        if row[47] not in dic[row[1]][-1]:
+            dic[row[1]][-1].append(row[47])
+        time = row[0]
+
+    # print dic
+    return dic
--- Homework3/Homework3-2/hw3-2.py
+++ Homework3/Homework3-2/hw3-2.py
@@ -1,3 +1,8 @@
 """
     Created by AMXPC on 2017/4/27.
 """
+import ReadData
+
+if __name__ == '__main__':
+    dic = ReadData.readcsv_withtime("../data/new4gtrain.csv")
+    dic = ReadData.readcsv_notime("../data/new4gtrain.csv")
