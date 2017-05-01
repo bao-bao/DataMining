71feab8708b5cbd35a755bd5fb83a7854430d62b
包天宇 <1452765@tongji.edu.cn>
2017-05-01 17:25:25 +0800

    SPADE done

--- Homework3/Homework3-2/hw3-2.py
+++ Homework3/Homework3-2/hw3-2.py
@@ -11,8 +11,8 @@ import time
 if __name__ == '__main__':
     # dic = ReadData.readcsv_withtime("../data/new4gtrain.csv")
     # dic = ReadData.readcsv_notime("../data/new4gtrain.csv")
-    # dic = TestData.gettest()
-    dic = TestData.getTimeTest()
+    dic = TestData.gettest()
+    dic_t = TestData.getTimeTest()
 
     start = time.time()
     Afrequent, sup = Apriori.apriori(dataSet=dic, minSupport=2)
@@ -25,11 +25,17 @@ if __name__ == '__main__':
     FTime = end - start
 
     start = time.time()
-    Gfrequent = GSP.gsp(dataSet=dic, minsupport=2)
+    Gfrequent = GSP.gsp(dataSet=dic_t, minsupport=2)
     end = time.time()
     GTime = end - start
+
+    start = time.time()
+    Sfrequent = GSP.gsp(dataSet=dic_t, minsupport=2)
+    end = time.time()
+    STime = end - start
 
     print dic
     print "Apriori:  time %.3fs    FrequentSet: %s" % (ATime, Afrequent)
     print "FPGrowth: time %.3fs    FrequentSet: %s" % (FTime, Ffrequent)
     print "GSP:      time %.3fs    FrequentSet: %s" % (GTime, Gfrequent)
+    print "SPADE:    time %.3fs    FrequentSet: %s" % (STime, Gfrequent)
