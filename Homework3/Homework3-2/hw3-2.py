6db580c826bfb19e061f3f21fe5b321892d21781
包天宇 <1452765@tongji.edu.cn>
2017-05-01 13:46:08 +0800

    apriori and fpgrowth test

--- Homework3/Homework3-2/hw3-2.py
+++ Homework3/Homework3-2/hw3-2.py
@@ -4,11 +4,24 @@
 import ReadData
 import TestData
 import Apriori
-import FPgrowth
+import FPGrowth
+import time
 
 if __name__ == '__main__':
-    # dic = ReadData.readcsv_withtime("../data/new4gtrain.csv")
-    dic = TestData.gettest()
-    frequent, sup = Apriori.apriori(dataSet=dic, minSupport=2)
-
     # dic = ReadData.readcsv_notime("../data/new4gtrain.csv")
+    dic = TestData.gettest()
+
+    start = time.time()
+    Afrequent, sup = Apriori.apriori(dataSet=dic, minSupport=2)
+    end = time.time()
+    ATime = end - start
+
+    start = time.time()
+    Ffrequent = FPGrowth.FPgrowth(dataSet=dic, minsupport=2)
+    end = time.time()
+    FTime = end - start
+
+    print "Apriori:  time %.3fs    FrequentSet: %s" % (ATime, Afrequent)
+    print "FPGrowth: time %.3fs    FrequentSet: %s" % (FTime, Ffrequent)
+
+    # dic = ReadData.readcsv_withtime("../data/new4gtrain.csv")
